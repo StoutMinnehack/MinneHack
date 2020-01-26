690a60a4f568a95c2c444b2b8875fad491acc94c
Matthew Pomes <the10thwiz@gmail.com>
2020-01-26 02:47:30 -0600

    Move scripts into seperate files

--- src/public/js/gapi.js
+++ src/public/js/gapi.js
@@ -1,36 +1,19 @@
-/**
- * The Sign-In client object.
- */
-var auth2;
+function onSignIn(googleUser) {
+    console.log("Worked");
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
 
-/**
- * Initializes the Sign-In client.
- */
-var initClient = function() {
-    gapi.load('auth2', function() {
-        /**
-         * Retrieve the singleton for the GoogleAuth library and set up the
-         * client.
-         */
-        auth2 = gapi.auth2.init({
-            client_id: '1035970031587-o4n43bfbas594qhr60umt3vqq9tdqnjo.apps.googleusercontent.com'
-        });
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+}
 
-        // Attach the click handler to the sign-in button
-        auth2.attachClickHandler('signin-button', {}, onSuccess, onFailure);
-    });
-};
-
-/**
- * Handle successful sign-ins.
- */
-var onSuccess = function(user) {
-    console.log('Signed in as ' + user.getBasicProfile().getName());
-};
-
-/**
- * Handle sign-in failures.
- */
-var onFailure = function(error) {
+function onFail(error) {
     console.log(error);
 }
