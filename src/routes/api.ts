bbef4a13b88fe90526283b61369a8d171efc1215
Austin Scott <12504656+Austin-Scott@users.noreply.github.com>
2020-01-26 03:13:11 -0600

    added searchevents endpoint

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoConnector from '../MongoConnector'
 import mongoose from 'mongoose'
+import { getDistance } from 'geolib'
 
 const router = express.Router()
 
@@ -41,19 +42,67 @@ router.post('/addevent', (req, res) => {
             state: String,
             zip: String,
             latitude: Number,
-            logitude: Number
+            longitude: Number
         },
         participants: [String]
     }
 
     let newEvent = new mongoConnector.Event(req.body)
     newEvent.save((err: any) => {
-        if(err) {
+        if (err) {
             res.sendStatus(500)
         } else {
             res.sendStatus(200)
         }
     })
+})
+
+router.post('/searchevents', (req, res) => {
+    if(!mongoConnector.Event) { return }
+
+    interface SearchQuery {
+        area?: {
+            latitude: number,
+            longitude: number,
+            radius: number
+        },
+        city?: String,
+        state?: String,
+        zip?: String
+    }
+
+    let query: SearchQuery = req.body
+    if(query.area) {
+        mongoConnector.Event.find({}).exec((err, mres) => {
+            let result = mres.filter(value => {
+                if(!query.area || !value.location || !value.location.latitude || !value.location.longitude) return false
+                if (getDistance([query.area.latitude, query.area.longitude], [value.location.latitude, value.location.longitude]) < query.area.radius) {
+                    return true
+                }
+                return false
+            })
+            res.json(result)
+            return
+        })
+    }
+    else if(query.city) {
+        mongoConnector.Event.find({ location: { city: query.city } }).exec((err, mres) => {
+            res.json(mres)
+            return
+        })
+    }
+    else if(query.state) {
+        mongoConnector.Event.find({ location: { state: query.state } }).exec((err, mres) => {
+            res.json(mres)
+            return
+        })
+    }
+    else if(query.zip) {
+        mongoConnector.Event.find({ location: { zip: query.zip } }).exec((err, mres) => {
+            res.json(mres)
+            return
+        })
+    }
 })
 
 export default router
