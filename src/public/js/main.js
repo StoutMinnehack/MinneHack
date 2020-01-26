56499015f821c2d5cd0eae9065f96ba46739fc9b
Matthew Pomes <the10thwiz@gmail.com>
2020-01-25 21:52:26 -0600

    Added JS script to HTML

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -23,9 +23,19 @@ $.get({
         "token": "XXX"
     },
     success: function(data, status, ctx) {
-        // A bunch of filling
+        console.log(data);
+        let tmp = $("#newsfeed");
+        for (let i in data.events) {
+            // A bunch of filling
+            tmp.append("<div>\
+                <h2>" + data.events[i].title + "</h2>\
+                <p>" + data.events[i].desc + "</p>\
+            </div>");
+        }
     },
     error: function(ctx, status, error) {
         // Same filling, but with login info
+        console.log("Error: " + status + ": " + error);
+        console.log(ctx);
     }
 })
