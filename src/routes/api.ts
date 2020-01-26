4b4b58b4280a19a8b1c26a0368377b8885fa240a
Austin Scott <12504656+Austin-Scott@users.noreply.github.com>
2020-01-25 21:45:04 -0600

    /api/events now responds with mock event data

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -5,4 +5,23 @@ router.get('/', (req, res) => {
     res.send('Welcome to the api route')
 })
 
+router.get('/events', (req, res) => {
+    res.send(JSON.stringify({
+        events: [
+            {
+                title: 'Mock event one',
+                desc: 'This is the first mock event'
+            },
+            {
+                title: 'Mock event two',
+                desc: 'This is the second mock event'
+            },
+            {
+                title: 'Mock event three',
+                desc: 'This is the third mock event'
+            }
+        ]
+    }))
+})
+
 export default router
