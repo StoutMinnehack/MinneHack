1e9ade24c0d9c6b3c21c5c1a6b22fe8edb141775
Matthew Pomes <the10thwiz@gmail.com>
2020-01-25 20:17:48 -0600

    Add events

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,19 +1,31 @@
-
-
 // Get User Profile
 
 // Check Browser Storage API for Token
 
 // Profile request
 $.get({
-  url: "/api/profile",
-  headers: {
-      "token": "XXX"
-  },
-  success: function(data, status, ctx) {
+    url: "/api/profile",
+    headers: {
+        "token": "XXX"
+    },
+    success: function(data, status, ctx) {
+        // A bunch of filling
+    },
+    error: function(ctx, status, error) {
+        // Same filling, but with login info
+    }
+})
 
-  },
-  error: function(ctx, status, error) {
-
-  }
+// Feed Request
+$.get({
+    url: "/api/events",
+    headers: {
+        "token": "XXX"
+    },
+    success: function(data, status, ctx) {
+        // A bunch of filling
+    },
+    error: function(ctx, status, error) {
+        // Same filling, but with login info
+    }
 })
